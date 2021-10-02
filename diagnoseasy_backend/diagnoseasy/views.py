1ebbea3d97852799953c3ae5d5554aaeac96d272
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2021-10-02 04:39:21 +0200

    CSV to JSON and back to CSV, because we hate optimization

--- diagnoseasy_backend/diagnoseasy/views.py
+++ diagnoseasy_backend/diagnoseasy/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @csrf_exempt
 def get_correlations(request):
 
-    person_condition = request.GET.get('condition', '')  # Field name might change, 'condition' is a placeholder
-    person_info = request.POST.get('info', '')  # Field name might change, 'info' is a placeholder
+    request_body = json.loads(request.body)
+    person_info = request_body['info']
+
+    print(person_info)
+
+    # Could use some library, but it's 4:30AM so I'm not doing that
+    csv_data = ''
+    for f in person_info:
+        csv_data += person_info[f] + ','
+    csv_data = csv_data[:-1]
+
+    print(csv_data)
+
 
     # Execute function based on condition, get and return results (i.e. possible correlations/conditions)
 
